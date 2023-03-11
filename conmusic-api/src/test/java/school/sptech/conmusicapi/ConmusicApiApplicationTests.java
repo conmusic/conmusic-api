8fecd16f1dbe5a1cfa763322a143e1c264f22f5b
VS-Sousa <vinicius.sousa@sptech.school>
2023-03-11 17:58:17 -0300

    Criando estrutura SpringBoot

---
+++ conmusic-api/src/test/java/school/sptech/conmusicapi/ConmusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.conmusicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConmusicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
