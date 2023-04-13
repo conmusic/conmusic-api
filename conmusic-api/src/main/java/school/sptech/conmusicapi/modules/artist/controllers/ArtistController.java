1e84042cff9e75cf826a8f26213c38083d04a3eb
cleber2010 <cleber2020casagrande@gmail.com>
2023-04-12 21:28:12 -0300

    Corrigindo status code e importações

--- conmusic-api/src/main/java/school/sptech/conmusicapi/modules/artist/controllers/ArtistController.java
+++ conmusic-api/src/main/java/school/sptech/conmusicapi/modules/artist/controllers/ArtistController.java
@@ -3,7 +3,11 @@ package school.sptech.conmusicapi.modules.artist.controllers;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import school.sptech.conmusicapi.modules.artist.dtos.ArtistDto;
 import school.sptech.conmusicapi.modules.artist.dtos.CreateArtistDto;
 import school.sptech.conmusicapi.modules.artist.services.ArtistService;
@@ -21,7 +25,7 @@ public class ArtistController {
     @GetMapping
     public ResponseEntity<List<ArtistDto>> findAll() {
         List<ArtistDto> artists = artistService.findAll();
-        return ResponseEntity.ok().body(artists);
+        return ResponseEntity.status(200).body(artists);
     }
 
     @PostMapping
